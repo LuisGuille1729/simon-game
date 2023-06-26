f09c205b961205e73e4a033be83d04c4d3355cc5
LuisGuille1729 <67127381+LuisGuille1729@users.noreply.github.com>
2023-06-26 16:30:50 -0400

    Added high score

--- game.js
+++ game.js
@@ -6,11 +6,11 @@ const buttons = {
 }
 
 const sound = {
-    "green": new Audio('https://github.com/LuisGuille1729/simon-game/raw/main/sounds/green.mp3'),
-    "red": new Audio('https://github.com/LuisGuille1729/simon-game/raw/main/sounds/red.mp3'),
-    "yellow": new Audio('https://github.com/LuisGuille1729/simon-game/raw/main/sounds/yellow.mp3'),
-    "blue": new Audio('https://github.com/LuisGuille1729/simon-game/raw/main/sounds/blue.mp3'),
-    "wrong": new Audio('https://github.com/LuisGuille1729/simon-game/raw/main/sounds/wrong.mp3')
+    "green": new Audio('/sounds/green.mp3'),
+    "red": new Audio('/sounds/red.mp3'),
+    "yellow": new Audio('/sounds/yellow.mp3'),
+    "blue": new Audio('/sounds/blue.mp3'),
+    "wrong": new Audio('/sounds/wrong.mp3')
 }
 
 function nextSequence() {
@@ -44,11 +44,13 @@ let active = false;
 let acceptClick = false;
 let curPlace = 0;
 let playerClicks = []
+let highScore = 0;
 
-document.addEventListener("keydown", function() {
+document.querySelector("body").addEventListener("keydown", function() {
     if (active) return; // do nothing if already active
 
     document.body.classList.remove("game-over");
+    document.querySelector("#score").classList.add("hidden") // do not display score as to not distract
     curSequence = [nextSequence()]; //start the sequence
     active = true;
     text.textContent = "Level 1"
@@ -72,10 +74,13 @@ for (let btn in buttons) {
             text.textContent = "Game Over!"
             sound["wrong"].play();
             active = false;
+            highScore = curSequence.length - 1 > highScore ? curSequence.length - 1 : highScore;
+            document.querySelector("#score").textContent = `High Score: ${highScore}`;
             setTimeout(() => {
                 if (!active) {
                 document.body.classList.remove("game-over");
                 text.textContent = "Press Key to Start"
+                document.querySelector("#score").classList.remove("hidden")
                 }
             }, 1500)
             
@@ -94,60 +99,4 @@ for (let btn in buttons) {
         }
     })   
 }
-
-/****ALLOW FOR MOBILE COMPABILITY****/
-
-document.addEventListener("touchstart", function() {
-    if (active) return; // do nothing if already active
-
-    document.body.classList.remove("game-over");
-    curSequence = [nextSequence()]; //start the sequence
-    active = true;
-    text.textContent = "Level 1"
-    playerClicks = []
-
-    setTimeout(() => blink(curSequence[0]), 350)
-    curPlace = 0;
-    acceptClick = true;
-})
-
-for (let btn in buttons) {
-    buttons[btn].addEventListener("touchstart", function() {
-        if (!active || !acceptClick) return;    // do nothing if not active and not accepting clicks
-
-        playerClicks.push(btn);
-    
-        pressed(btn);
-    
-        if (playerClicks[curPlace] !== curSequence[curPlace]) { // if incorrect button pressed, then finish game
-            document.body.classList.add("game-over");
-            text.textContent = "Game Over!"
-            sound["wrong"].play();
-            active = false;
-            setTimeout(() => {
-                if (!active) {
-                document.body.classList.remove("game-over");
-                text.textContent = "Press Key to Start"
-                }
-            }, 1500)
-            
-        }
-        else if (curPlace === curSequence.length-1) {    // finished level, go to next level
-            text.textContent = `Level ${curPlace+2}` 
-            const newColor = nextSequence()
-            curSequence.push(newColor)
-            for (let i = 0; i < curSequence.length; i++) {
-            setTimeout(() => blink(curSequence[i]), 500*i + 500)
-            }
-            curPlace = 0
-            playerClicks = []
-        } else {    // otherwise, correct button was pressed, check next
-            curPlace++
-        }
-    })   
-}
-
-
-
-
 
